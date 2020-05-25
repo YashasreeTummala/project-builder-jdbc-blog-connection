7bf67c616a833993c0bbb1b1839c6008950302a3
yashasree <yashasreeaditya@gmail.com>
2020-05-26 00:27:15 +0530

    FSJ0420A105

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -28,7 +28,20 @@ public class SignUpController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Sign up Controller");
-		// Fill your code here
+		
+		Connection con = null;
+		try {
+			con = ConnectionManager.getConnection();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(con != null)
+			System.out.println("Connection Established");
+		else
+			System.out.println("Connection not established");
+		
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
 		rd.forward(request,response);
 	}
